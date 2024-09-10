6f0bacba72667f0e09121368a084db3ffc74e0ae
Dilip Sahoo <66472615+dilipkumarsahoo@users.noreply.github.com>
2024-09-11 00:11:58 +0530

    basic of context of react

---
+++ 08minicontext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile(){
+    const {user} = useContext(UserContext)
+    
+    if (!user)  return <div>Please login</div>
+    
+    return   <h1>Welcome {user.username}</h1>
+    
+}
+
+export default Profile
