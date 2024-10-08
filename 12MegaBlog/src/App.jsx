af427d741f5b2685595b963de270c18038ca2ab1
Dilip Sahoo <66472615+dilipkumarsahoo@users.noreply.github.com>
2024-10-08 08:09:33 +0530

    create react component

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,19 +1,40 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import  authService  from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer} from './components'
 
 function App() {
-  //IF YOU CREATE USING CREATE REACT
-  // console.log(process.env.REACT_APP_APPWRITE_URL);
-  
-  //IF YOU CREATE USING VITE
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
-  return (
+  return !loading ? (
     <>
-      <h1>A blog app with appwrite</h1>
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            TODO: {/* < Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null
 }
 
 export default App
